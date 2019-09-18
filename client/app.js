1d878458bff7f084a62c9aacaed849f6f947886b
ThePhiri <chipoephiri@gmail.com>
2019-09-18 07:16:20 +0200

    Add update functionality

--- client/app.js
+++ client/app.js
@@ -11,6 +11,8 @@ function ViewModel(){
     self.goalInputType = ko.observable();
     self.goalInputDeadline = ko.observable();
     self.selectedGoals = ko.observableArray();
+    self.isUpdate = ko.observable(false);
+    self.updateId = ko.observable();
     self.canEdit = ko.computed(function(){
         return self.selectedGoals().length > 0;
     });
@@ -44,6 +46,51 @@ function ViewModel(){
                 }
             });
     }
+
+    self.updateGoal = function(){
+        var id = self.updateId;
+        var name = $('#name').val();
+        var type = $('#type').val();
+        var deadline = $('#deadline').val();
+
+        self.goals.remove(function(item){return item._id == id});
+
+        self.goals.push({
+            name: name,
+            type: type,
+            deadline: deadline
+        });
+
+        $.ajax(
+            {
+                url: "http://localhost:3000/goals/"+id,
+                data: JSON.stringify({
+                    "name": name,
+                    "type": type,
+                    "deadline": deadline
+                }),
+                type: "PUT",
+                contentType: "application/json",
+                success: function(data){
+                    console.log('Goal Updated...');
+                },
+                error: function(xhr, status, err){
+                    console.log(err);
+                }
+            });
+    }
+    self.editSelected = function(){
+        self.updateId = self.selectedGoals()[0]._id;
+        var name = self.selectedGoals()[0].name;
+        var type = self.selectedGoals()[0].type;
+        var deadline = self.selectedGoals()[0].deadline;
+
+        self.isUpdate(true);
+        self.goalInputName(name);
+        self.goalInputDeadline(deadline);
+        self.goalInputType(type);
+    }
+
     self.deleteSelected = function(){
         $.each(self.selectedGoals(), function(index, value){
             var id = self.selectedGoals()[index]._id;
@@ -64,7 +111,7 @@ function ViewModel(){
         self.selectedGoals.removeAll()
     }
 
-    self.types = ko.observableArray(['Health & Fitness', 'Professional', 'Relationships', 'Monetary'])
+    self.types = ko.observableArray(['Health & Fitness', 'Professional', 'Relationships', 'Monetary', 'Saving'])
 }
 
 var viewModel = new ViewModel();
