1507ee516fd1520a691e896b4d33d727111afa83
ThePhiri <chipoephiri@gmail.com>
2019-09-29 20:28:42 +0200

    changed port

--- app.js
+++ app.js
@@ -63,5 +63,7 @@ app.delete('/goals/:id', function(req, res){
 	});
 });
 
-app.listen(3000);
-console.log('running on port 3000...');
+var server = app.listen(process.env.PORT || 5000), function() {
+	var port = server.address().port;
+	console.log("Express is working on port " + port);
+}
